660656a9a56e09318a84a16b0b817d6d405087b2
Dong <1361840595@qq.com>
2020-07-28 17:03:47 +0800

    [1-Build basic API in Spring-Boot] [Dong and Sam] feature : implement EmployeeServiceImpl.addEmployee() and EmployeeServiceImpl.findAllEmployees()

--- src/main/java/com/thoughtworks/springbootemployee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/impl/EmployeeServiceImpl.java
@@ -2,7 +2,9 @@ package com.thoughtworks.springbootemployee.service.impl;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,14 +13,19 @@ import java.util.List;
  * @Version 1.0
  */
 public class EmployeeServiceImpl implements EmployeeService {
+    List<Employee> employees = new ArrayList<>();
+
     @Override
     public void addEmployee(Employee employee) {
-
+        this.employees.add(employee);
     }
 
     @Override
     public Employee findEmployeeById(String employeeId) {
-        return null;
+        if(StringUtils.isEmpty(employeeId)){
+            return null;
+        }
+        return this.employees.stream().filter(employee -> employeeId.equals(employee.getId())).findFirst().get();
     }
 
     @Override

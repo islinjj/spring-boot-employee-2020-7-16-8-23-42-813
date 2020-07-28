fb6f0bd73aac27ed74ff27c9a3fb339ae9133f2e
Dong <1361840595@qq.com>
2020-07-28 16:48:26 +0800

    [1-Build basic API in Spring-Boot] [Dong and Sam] refactor : delete useless class and create a EmployeeController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author ZHUDO2
+ * @Date 7/28/2020 4:45 PM
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+}

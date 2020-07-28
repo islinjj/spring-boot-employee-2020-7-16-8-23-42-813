97ae66837e1d9d5dc8dd6aca8b21df29e2da0023
Dong <1361840595@qq.com>
2020-07-28 17:14:17 +0800

    [1-Build basic API in Spring-Boot] [Dong and Sam] feature : implement the function of getAllEmployees

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @Author ZHUDO2
  * @Date 7/28/2020 4:45 PM
@@ -19,5 +21,10 @@ public class EmployeeController {
     @PostMapping()
     public void addEmployee(@RequestBody Employee employee) {
         employeeService.addEmployee(employee);
+    }
+
+    @GetMapping()
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAllEmployees();
     }
 }

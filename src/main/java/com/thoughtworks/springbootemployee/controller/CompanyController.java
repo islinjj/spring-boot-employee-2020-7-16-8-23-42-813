81ee9b9af5ceb50188e2a4b6963560cd94a94bef
Dong <1361840595@qq.com>
2020-07-28 20:10:21 +0800

    [1-Build basic API in Spring-Boot] [Dong and Sam] refactor : add an annotation in CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -3,10 +3,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,8 +22,9 @@ public class CompanyController {
     public List<Company> findAllCompanies() {
         return companyService.findAllCompanies();
     }
+
     @PostMapping
-    public void addCompany(Company company){
+    public void addCompany(@RequestBody Company company) {
         companyService.addCompany(company);
     }
 }

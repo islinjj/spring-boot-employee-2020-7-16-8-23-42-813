2770378a35e85b8770bd97c6c5399af7e35432dd
Dong <1361840595@qq.com>
2020-07-28 19:50:47 +0800

    [1-Build basic API in Spring-Boot] [Dong and Sam] feature : add a CompanyService the add some abstract methods in it

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+
+import java.util.List;
+
+/**
+ * @Author ZHUDO2
+ * @Date 7/28/2020 7:42 PM
+ * @Version 1.0
+ */
+public interface CompanyService {
+    List<Company> findAllCompanies();
+    Company findCompanyById(int companyId);
+    List<Employee> findEmployeesByCompanyId(int companyId);
+    void addCompany(Company company);
+    List<Company> getCompaniesByPage();
+    void updateCompany(Company company);
+    void deleteEmployeesByCompanyId(int companyId);
+}

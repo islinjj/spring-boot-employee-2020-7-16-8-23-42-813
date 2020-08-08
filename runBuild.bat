beb0902af30091dbb59b243d811bef80f195027f
SAM Y LI (ITA-ISDC-ISD-OOCLL/ZHA) <LISA8@OOCL.COM>
2020-08-08 09:57:59 +0800

    update

--- runBuild.bat
+++ runBuild.bat
@@ -1,4 +1,4 @@
 cd ../
-xcopy spring-boot-test d:/deploy/spring-boot-test /e /y
-cd d:/deploy/spring-boot-test
+xcopy "spring-boot-test" "d:\\deploy\\spring-boot-test" /e /y
+cd d:\\deploy\\spring-boot-test
 gradle clean build -x test
